532c9ed252e2a4d1e6c6908db903b6e11cd45625
Nhikoh <nicola.parisi001@studenti.uniparthenope.it>
2025-09-09 15:26:17 +0200

    Indicator Class

--- JS/Main.js
+++ JS/Main.js
@@ -1,41 +1,52 @@
-const NumericButtonID = document.getElementById("CalendarDaysID").querySelectorAll("div");
-console.log(NumericButtonID);
-const IndicatorPagination = document.getElementById("IndicatorPagin");
-const ActivePaginationIndicator = document.querySelector(".Active");
-const IndicatorPaginationPos = IndicatorPagination.getBoundingClientRect();
+class Indicator{
+    constructor(ElementsID,ElementsType,IndicatorObjectID,DefaultActiveObject){
+        this.Elements = document.getElementById(ElementsID).querySelectorAll(ElementsType);
+        this.Indicator = document.getElementById(IndicatorObjectID);
+        this.ActiveObject = document.querySelector(DefaultActiveObject);
+        this.RectActiveObject = this.ActiveObject.getBoundingClientRect();
+        this.IndicatorPosition = this.Indicator.getBoundingClientRect();
+    }
+    SetOnActive(){
+        let offset = this.ActiveObject.x - this.IndicatorPosition.x;
+        let offsetY = this.ActiveObject.y - this.IndicatorPosition.y + 10;
+        this.ActiveObject.style.transform = "translate("+offset+"px,"+offsetY+"px)";
+    }
+    HoverMouse(){
+        this.Elements.forEach((Element) =>{
+            Element.addEventListener("mouseover", () =>{
+                const ElementPos = Element.getBoundingClientRect();
+                let offset = ElementPos.x - this.IndicatorPosition.x;
+                let offsetY = ElementPos.y - this.IndicatorPosition.y + 10;
+                this.ActiveObject.style.backgroundColor = "transparent";
+                this.Indicator.style.visibility = "visible";
+                this.Indicator.style.transform = "translate("+offset+"px,"+offsetY+"px)";
+                this.Indicator.style.transition = "0.1s ease-in-out"; 
+            })
+        })
+    }
+    NotHoverMouse(){
+        this.Elements.forEach((Element) =>{
+            Element.addEventListener("mouseout", () =>{
+            let offset = this.RectActiveObject.x - this.IndicatorPosition.x;
+            let offsetY = this.RectActiveObject.y - this.IndicatorPosition.y + 10;
 
-const NumericButtonIDActive = document.querySelector(".Active");
+            this.Indicator .style.visibility = "hidden";
+            this.Indicator .style.transform = "translate("+offset+"px,"+offsetY+"px)";
+            this.Indicator .style.transition = "0.3s ease-in-out"; 
+            if((this.IndicatorPosition.x+(this.ActiveObject.x-this.IndicatorPosition.x)) ===  this.ActiveObject.x){
+                ActivePaginationIndicator.style.backgroundColor = "hsl(0,0%,70%,0.3)";
+            }
+            if((this.IndicatorPosition.y+(this.ActiveObject.y-this.IndicatorPosition.y)) ===  this.ActiveObject.y){
+                ActivePaginationIndicator.style.backgroundColor = "hsl(0,0%,70%,0.3)";
+            }
+        })
+})
+    }
+}
+const IndicatorCalendar = new Indicator("CalendarDaysID","div","IndicatorePagina",".Active");
+IndicatorCalendar.HoverMouse();
+IndicatorCalendar.NotHoverMouse();
+
 addEventListener("DOMContentLoaded", () =>{
-    const ActivePos = NumericButtonIDActive.getBoundingClientRect();
-    let offset = ActivePos.x - IndicatorPaginationPos.x;
-    let offsetY = ActivePos.y - IndicatorPaginationPos.y + 10;
-    IndicatorPagination.style.transform = "translate("+offset+"px,"+offsetY+"px)";
-})
-NumericButtonID.forEach((NumericButton) =>{
-    NumericButton.addEventListener("mouseover", () =>{
-        const NumericButtonIDPos = NumericButton.getBoundingClientRect();
-        let offset = NumericButtonIDPos.x - IndicatorPaginationPos.x;
-        let offsetY = NumericButtonIDPos.y - IndicatorPaginationPos.y + 10;
-        ActivePaginationIndicator.style.backgroundColor = "transparent";
-        IndicatorPagination.style.visibility = "visible";
-        IndicatorPagination.style.transform = "translate("+offset+"px,"+offsetY+"px)";
-        IndicatorPagination.style.transition = "0.1s ease-in-out"; 
-    })
-})
-NumericButtonID.forEach((NumericButton) =>{
-    NumericButton.addEventListener("mouseout", () =>{
-        const ActivePos = NumericButtonIDActive.getBoundingClientRect();
-        let offset = ActivePos.x - IndicatorPaginationPos.x;
-        let offsetY = ActivePos.y - IndicatorPaginationPos.y + 10;
-
-        IndicatorPagination.style.visibility = "hidden";
-        IndicatorPagination.style.transform = "translate("+offset+"px,"+offsetY+"px)";
-        IndicatorPagination.style.transition = "0.3s ease-in-out"; 
-        if((IndicatorPaginationPos.x+(ActivePaginationIndicatorPos.x-IndicatorPaginationPos.x)) ===  ActivePaginationIndicatorPos.x){
-            ActivePaginationIndicator.style.backgroundColor = "hsl(0,0%,70%,0.3)";
-        }
-        if((IndicatorPaginationPos.y+(ActivePaginationIndicatorPos.y-IndicatorPaginationPos.y)) ===  ActivePaginationIndicatorPos.y){
-            ActivePaginationIndicator.style.backgroundColor = "hsl(0,0%,70%,0.3)";
-        }
-    })
+    IndicatorCalendar.SetOnActive();
 })
